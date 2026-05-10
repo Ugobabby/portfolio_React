848b0da6c905889b939511f722a192b48e579217
Ugobabby <udeaghagrace007@gmail.com>
2026-05-10 22:37:33 +0100

    i switched back the theme, set dark theme as default and white theme as change

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -3,14 +3,14 @@ import React, { createContext, useState, useEffect } from "react";
 export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState("dark");
 
   useEffect(() => {
     document.body.className = theme;
   }, [theme]);
 
   const toggleTheme = () => {
-    setTheme((prev) => (prev === "light" ? "dark" : "light"));
+    setTheme((prev) => (prev === "dark" ? "light" : "dark"));
   };
 
   return (
